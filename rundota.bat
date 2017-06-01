3de23d30b8c699bb57e45cbcff3ada7561620197
Dylan Baker <dylan@pnwbakers.com>
2017-06-01 15:10:25 -0700

    rundota.bat: Add PTS tracing

--- rundota.bat
+++ rundota.bat
@@ -5,11 +5,20 @@ REM solution)
 SET DOTA2_DIR=C:/Program Files (x86)/Steam/steamapps/common/dota 2 beta
 SET DOTA2_BIN=%DOTA2_DIR%/game/bin/win64/dota2.exe
 SET DOTA2_BENCH_CSV=%DOTA2_DIR%/game/dota/Source2Bench.csv
+
+REM Use the valve trace
 SET DOTA_TRACE_FILE=2203598540
+SET START_FRAME=80000
+SET STOP_FRAME=85000
+
+REM Use the PTS trace
+REM DOTA_TRACE_FILE=1971360796
+REM START_FRAME=50000
+REM STOP_FRAME=51000
 
 for %%x in (dx9 dx11 gl vk) do (
     for /l %%_ in (1, 1, 5) do (
-        "%DOTA2_BIN%" -%%x  -autoconfig_level 3 +fps_max 0  -fs -w 1920 -h 1080 +cl_showfps 2 +timedemo %DOTA2_TRACE_FILE% +timedemo_start 80000 +timedemo_end 85000 -testscript_inline "Test_WaitForCheckPoint DemoPlaybackFinished; quit"
+        "%DOTA2_BIN%" -%%x  -autoconfig_level 3 +fps_max 0  -fs -w 1920 -h 1080 +cl_showfps 2 +timedemo %DOTA2_TRACE_FILE% +timedemo_start %START_FRAME% +timedemo_end %STOP_FRAME% -testscript_inline "Test_WaitForCheckPoint DemoPlaybackFinished; quit"
     )
 )
 
