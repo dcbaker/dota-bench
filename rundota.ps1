894256dffb06562176fd55f5301331703190983e
Dylan Baker <dylan@pnwbakers.com>
2017-06-07 11:41:40 -0700

    rundota.ps1: fix equiality operators and variable typo

--- rundota.ps1
+++ rundota.ps1
@@ -8,7 +8,7 @@ $DOTA2_DIR = "C:/Program Files (x86)/Steam/steamapps/common/dota 2 beta"
 $DOTA2_BIN = "$DOTA2_DIR/game/bin/win64/dota2.exe"
 $DOTA2_BENCH_CSV = "$DOTA2_DIR/game/dota/Source2Bench.csv"
 
-if ($benchmark == 'valve') {
+if ($benchmark -eq 'valve') {
     $DOTA2_TRACE_FILE = 2203598540
     $START_TIME = 80000 
     $END_TIME = 85000
@@ -19,8 +19,8 @@ if ($benchmark == 'valve') {
 }
 
 foreach ($api in "dx9", "dx10", "gl", "vk") {
-    for ($i = 0; $i < 5; $i++) {
-        "$DOATA2_BIN" "-$api" -autoconfig_level 3 +fps_max 0 -fs -w 1920 -h 1080 +cl_showfps 2 +timedemo $DOTA2_TRACE_FILE +timedemo_start $START_TIME +timedemo_end $END_TIME -testscript_inline "Test_WaitForCheckPoint DemoPlaybackFinished; quit"
+    for ($i = 0; $i -lt 5; $i++) {
+        "$DOTA2_BIN" "-$api" -autoconfig_level 3 +fps_max 0 -fs -w 1920 -h 1080 +cl_showfps 2 +timedemo $DOTA2_TRACE_FILE +timedemo_start $START_TIME +timedemo_end $END_TIME -testscript_inline "Test_WaitForCheckPoint DemoPlaybackFinished; quit"
         Write-Host -nonewline "%api: "
         Get-Content $DOATA2_BENCH_CSV -Tail 1 | %{ $_.Split(" ",[StringSplitOptions]"RemoveEmptyEntries")[$columnToGet] }
     }
